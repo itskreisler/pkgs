bbd4f30f386da37b9258b3a6caac6e9cf0ac43e6
Kreisler Ramirez <itskreisler@outlook.com>
2024-05-09 15:53:19 -0500

    Actualizar pruebas de debounce en test.spec.cjs y test.spec.mjs

--- packages/debounce/src/lib/debounce.ts
+++ packages/debounce/src/lib/debounce.ts
@@ -1,19 +1,19 @@
 export type T = any
-export type Tfn = (...args: T[]) => T;
+export type TArrowFunction = (...args: T[]) => T;
 export type TFns = {
   immediate: boolean
-  onCall: Tfn
-  onComplete: Tfn
+  onCall: Function
+  onComplete: Function
   flood: number
-  onFlood: Tfn
+  onFlood: Function
 }
 /**
  * @typedef {Object} Fns
- * @property {boolean} immediate If true, function will be called immediately
- * @property {Function} onCall Function to call when debounce function is called
- * @property {Function} onComplete Function to call when debounce function is completed
- * @property {number} flood Number of calls to make before calling onFlood function
- * @property {Function} onFlood Function to call when flood limit is reached
+ * @property {boolean} [immediate] If true, function will be called immediately
+ * @property {Function} [onCall] Function to call when debounce function is called
+ * @property {Function} [onComplete] Function to call when debounce function is completed
+ * @property {number} [flood] Number of calls to make before calling onFlood function
+ * @property {Function} [onFlood] Function to call when flood limit is reached
  */
 
 /**
@@ -21,46 +21,53 @@ export type TFns = {
  * @example
  * const consoleLogDebounced = debounce(console.log, 1000, {
  *  immediate: true,
- *  onCall: () => console.log('Calling debounce function'),
- *  onComplete: () => console.log('Debounce function completed'),
+ *  onCall: (...args) => console.log('Calling debounce function'),
+ *  onComplete: (...args) => console.log('Debounce function completed'),
  *  flood: 7,
- *  onFlood: () => console.log('Flood limit reached')
+ *  onFlood: (...args) => console.log('Flood limit reached')
  * })
  * @param {Function} func Function to debounce
  * @param {number} msWait Number of milliseconds to wait before calling function
- * @param {Fns} fns Object with functions to call
+ * @param {TFns} fns Object with functions to call
  * @returns {Function} Debounce function
  */
 export function debounce(
-  func: Tfn,
-  msWait: number = 1000,
-  fns: TFns = {
-    onCall: function () { },
-    onComplete: function () { },
-    onFlood: function () { },
-    immediate: false,
-    flood: 7
+  func: Required<Function>,
+  msWait: Required<number>,
+  fns: Partial<TFns> = {
+    immediate: undefined,
+    onCall: undefined,
+    onComplete: undefined,
+    flood: undefined,
+    onFlood: undefined
   }
-): Tfn {
+): Function {
   let timeout: NodeJS.Timeout
-  let callCount = 0
+  let callCount: number = 0
   const { onCall, onComplete, flood, onFlood, immediate } = fns
-
+  //
+  const isFunction = (fn: T): boolean => typeof fn === 'function'
+  const isNotUndefined = (val: T): boolean => typeof val !== "undefined"
+  //
   return function (...args: T) {
-    const context: T = this
+    const context = this // as unknown as ThisParameterType<typeof _>
     clearTimeout(timeout)
 
     if (immediate && callCount === 0) {
       func.apply(context, args)
       callCount++
     } else {
-      if (onCall && typeof onCall === 'function') {
+      if (onCall && isFunction(onCall)) {
         onCall.apply(context, args)
       }
 
       callCount++
 
-      if (callCount % flood === 0 && onFlood && typeof onFlood === 'function') {
+      if (flood &&
+        isNotUndefined(flood) &&
+        callCount % flood === 0 &&
+        onFlood &&
+        isFunction(onFlood)) {
         onFlood.apply(context, args)
       }
 
@@ -68,7 +75,7 @@ export function debounce(
         func.apply(context, args)
         callCount = 0 // callCount % flood
 
-        if (onComplete && typeof onComplete === 'function') {
+        if (onComplete && isFunction(onComplete)) {
           onComplete.apply(context, args)
         }
       }, msWait)
--- packages/debounce/test/test.spec.mjs
+++ packages/debounce/test/test.spec.mjs
@@ -9,13 +9,81 @@ import { describe, it } from 'node:test'
 import assert from 'node:assert'
 
 // » IMPORT MODULES
-import main from '../dist/index.mjs'
+import { debounce } from '../dist/index.mjs'
 
 
 // ━━ TEST ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 describe('TESTING', async () => {
-  it('Hello World', async () => {
-    const result = main()
-    assert.strictEqual(result, 'Hello World!')
-  })
+  it('should debounce function calls', () => {
+    const mockFunction = () => { };
+    const debouncedFunction = debounce(mockFunction, 1000);
+
+    // Simular el paso del tiempo
+    setTimeout(() => {
+      debouncedFunction();
+    }, 500);
+
+    setTimeout(() => {
+      debouncedFunction();
+    }, 900);
+
+    setTimeout(() => {
+      debouncedFunction();
+    }, 1300);
+
+    setTimeout(() => {
+      // Verificar que la función solo se haya llamado una vez
+      assert.strictEqual(debouncedFunction(), undefined);
+    }, 2000);
+  });
+
+  it('should call onCall function if provided', () => {
+    const mockFunction = () => { };
+    let called = false;
+    const onCallFunction = () => {
+      called = true;
+    };
+    const debouncedFunction = debounce(mockFunction, 1000, { onCall: onCallFunction });
+
+    // Llamar a la función debounce
+    debouncedFunction();
+
+    // Verificar que la función onCall haya sido llamada
+    assert.strictEqual(called, true);
+  });
+  it('should call onComplete function if provided', () => {
+    const mockFunction = () => { };
+    let completed = false;
+    const onCompleteFunction = () => {
+      completed = true;
+    };
+    const debouncedFunction = debounce(mockFunction, 1000, { onComplete: onCompleteFunction });
+
+    // Simular llamadas a la función debounce
+    debouncedFunction();
+    debouncedFunction();
+
+    // Avanzar el tiempo lo suficiente para que se complete la función debounce
+    setTimeout(() => {
+      // Verificar que la función onComplete haya sido llamada
+      assert.strictEqual(completed, true);
+    }, 1500);
+  });
+  it('should call onFlood function if flood limit is reached', () => {
+    const mockFunction = () => { };
+    let floodCalled = false;
+    const onFloodFunction = () => {
+      floodCalled = true;
+    };
+    const debouncedFunction = debounce(mockFunction, 1000, { flood: 3, onFlood: onFloodFunction });
+
+    // Llamar a la función debounce múltiples veces
+    debouncedFunction();
+    debouncedFunction();
+    debouncedFunction();
+    debouncedFunction();
+
+    // Verificar que la función onFlood haya sido llamada
+    assert.strictEqual(floodCalled, true);
+  });
 })
