bd42525501f7c9c2b7bcb7b0cb93a78ceb67fd8d
Kreisler Ramirez <itskreisler@outlook.com>
2024-09-22 17:50:31 -0500

    fix

--- packages/try-catch/tsup.config.ts
+++ packages/try-catch/tsup.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: {
-    '.': 'src/index.ts'
+    index: 'src/index.ts',
+    tryCatch: 'src/lib/try-catch.ts',
+    tryCatchFinally: 'src/lib/tryCatchFinally.ts',
+    tryFinally: 'src/lib/tryFinally.ts',
+    tryToCatch: 'src/lib/tryToCatch.ts'
   },
   banner: {
     js: '// @buymeacoffee https://www.buymeacoffee.com/kreisler'
