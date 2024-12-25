3668415746af555b9d703680d365584e8606ead8
Kreisler Ramirez <itskreisler@outlook.com>
2024-12-25 18:23:59 -0500

    feat: actualizar nombre de usuario del bot y mejorar la configuración del entorno

--- packages/bot-tl-ts/src/bot/helpers/env.ts
+++ packages/bot-tl-ts/src/bot/helpers/env.ts
@@ -20,14 +20,17 @@ export interface IprocessEnv extends NodeJS.ProcessEnv {
 export const {
   TELEGRAM_TOKEN_PROD = String(),
   TELEGRAM_TOKEN_DEV = String(),
-  BOT_USERNAME = 'uwunubot',
+  BOT_USERNAME = 'username',
   BOT_PREFIX = '/',
   NODE_ENV = 'development',
   AUTHORIZED_USERS = ''
 } = process.env as IprocessEnv
 export const configEnv = () => ({
+  TELEGRAM_TOKEN_PROD,
+  TELEGRAM_TOKEN_DEV,
   BOT_USERNAME,
   BOT_PREFIX,
   NODE_ENV,
   AUTHORIZED_USERS
 })
+// console.log(process.env);
